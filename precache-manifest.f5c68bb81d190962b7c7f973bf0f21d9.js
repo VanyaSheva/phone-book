970f8074fe1dc76d4c504c10faf7452a5f02393d
Vanya Shevchuk <aggressor2000@gmail.com>
2020-05-19 17:38:08 +0300

    Updates

---
+++ precache-manifest.f5c68bb81d190962b7c7f973bf0f21d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2c8cd46e3523cc631844d82dec174c",
+    "url": "/phone-book/index.html"
+  },
+  {
+    "revision": "611edc6feaf90531ec5d",
+    "url": "/phone-book/static/css/main.75f51e5b.chunk.css"
+  },
+  {
+    "revision": "cafdd81adee21f6943ad",
+    "url": "/phone-book/static/js/2.15e57eeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/phone-book/static/js/2.15e57eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "611edc6feaf90531ec5d",
+    "url": "/phone-book/static/js/main.46d70360.chunk.js"
+  },
+  {
+    "revision": "65037630367181baed20",
+    "url": "/phone-book/static/js/runtime-main.3cbd65af.js"
+  }
+]);
